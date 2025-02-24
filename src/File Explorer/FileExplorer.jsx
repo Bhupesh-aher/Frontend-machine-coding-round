4044b7c217412a56261fd3b3ace3c51da090ff82
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-02-25 00:24:29 +0530

    add & remove file task

--- src/File Explorer/FileExplorer.jsx
+++ src/File Explorer/FileExplorer.jsx
@@ -6,11 +6,7 @@ const FileExplorer = () => {
 
     const [data, setData] = useState(json)
 
-    // List component will just render the list of objects/nodes
-    // it is recursing over list/array of objects(nodes)
-    const List = ({list}) => {
-
-
+    const List = ({list, addNodeToList, deleteNodeFromList}) => {
         const [isExpand, setIsExpand] = useState({})
 
         return (
@@ -19,7 +15,9 @@ const FileExplorer = () => {
                  <div key={node.id}>
                     {node.isFolder && <span onClick={() => setIsExpand(prev =>( {...prev, [node.name]: !prev[node.name] }))}>{isExpand?.[node.name] ? "-" : "+"  } </span> }
                     <span>{node.name}</span>
-                    {isExpand?.[node.name] && node?.children && <List list={node.children} />}
+                    {node?.isFolder && <span onClick={() => addNodeToList(node.id)}><img src="https://cdn-icons-png.flaticon.com/512/58/58298.png" alt="" className='icon'/></span>}
+                    <span onClick={() => deleteNodeFromList(node.id)}><img src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" alt="icon" className='icon'/></span>
+                    {isExpand?.[node.name] && node?.children && <List list={node.children}  addNodeToList={addNodeToList} deleteNodeFromList={deleteNodeFromList}/>}
                  </div>
                     
                 ))}
@@ -27,10 +25,55 @@ const FileExplorer = () => {
         )
     }
 
+
+    // adding node to a tree using recursion
+    const addNodeToList = (parentId) => {
+            
+    const name = prompt("Enter Folder Name")
+
+        const updateTree = (list) => {
+            return list.map((node) => {
+                if(node.id === parentId){
+                    return {
+                        ...node,
+                        children: [
+                            ...node.children, 
+                            {id: Date.now().toString(), name: name, isFolder: true, children: []}
+                        ],  
+                    }
+                }
+                if(node.children){
+                    return {...node, children: updateTree(node.children)}
+                }
+                return node
+            })
+        }
+        setData((prev) => updateTree(prev))  
+    }
+
+
+    // removing node to a tree using recursion
+
+    const deleteNodeFromList = (itemId) => {
+        const updateTree = (list) => {
+            return list
+                .filter((node) => node.id !== itemId)
+                .map((node) => {
+                    if(node.children){
+                        return {...node, children: updateTree(node.children)}
+                }
+                return node
+            })
+        }
+        setData((prev) => updateTree(prev))
+    }
+
     return (
+
+
         <div>
             <h1>File-Folder Explorer</h1>
-            <List list={data}/>
+            <List list={data} addNodeToList={addNodeToList} deleteNodeFromList={deleteNodeFromList}/>
         </div>
     )
 }
